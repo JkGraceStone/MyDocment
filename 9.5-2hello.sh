7dac17ede8239cba02536a1bd2f3a93dc69380c5
JkGraceStone <2458697069@qq.com>
2017-09-05 22:41:06 +0800

    9.5commit

---
+++ 9.5-2hello.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#the first program
+echo -e "\e[1;34m 天上人间 \e[0m"
+#把今天的学习的东西记录到文档中，提交今天的成果成：
+echo -e 'h\te\tl\tl\no\tworld'
+echo -e "\x40"
+echo 'shell 传递参数'
+echo "file name $0"
+echo "prameter one $1"
+echo "parmeter two $2"
